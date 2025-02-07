5ebdfd6007e088ebe9f4d874c3d723d3a049ea9a
K0nGbawa <2716692964@qq.com>
2025-02-07 15:27:06 +0800

    fixed bugs

--- midi2phi.py
+++ midi2phi.py
@@ -1,16 +1,20 @@
 import mido, sys, json, os.path, math
 from pydub import AudioSegment
+from PIL import Image, ImageDraw, ImageFont
+import zipfile
+import random, os
+import struct
 
 def midi_note2hz(note: int) -> float:
     return 440 * 2 ** ((note - 69) / 12)
 
-def get_time(time):
+def get_time(time, track):
     sec = 0.0
     beat = time
-    for i, e in enumerate(bpm_list):
+    for i, e in enumerate(bpm_list[track]):
         bpmv = e["bpm"]
-        if i != len(bpm_list) - 1:
-            et_beat = bpm_list[i + 1]["time"] - e["time"]
+        if i != len(bpm_list[track]) - 1:
+            et_beat = bpm_list[track][i + 1]["time"] - e["time"]
             if beat >= et_beat:
                 sec += et_beat / tpb * (60 / bpmv)
                 beat -= et_beat
@@ -25,7 +29,12 @@ if len(sys.argv) != 3:
     print("usage: python midi2phi.py <input-path> <output-path>")
     exit(1)
 
-mid = mido.MidiFile(sys.argv[1])
+with open(sys.argv[1], 'rb') as f:
+    data = f.read(14)
+
+data = struct.unpack(">4sI3H", data)
+
+mid = mido.MidiFile(sys.argv[1], type=data[2], ticks_per_beat=data[4])
 
 chart = {
         "formatVersion": 3,
@@ -74,7 +83,6 @@ chart = {
 
 tpb = mid.ticks_per_beat
 
-
 tempo = 100000000
 notes = []
 active_notes = {}
@@ -87,18 +95,21 @@ bpm_dict = {}
 current_time = 0.
 ticks_per_beat = mid.ticks_per_beat
 
+bpm_list = []
 print("获取bpm列表...")
 for track in mid.tracks:
     for msg in track:
+        current_time += msg.time
         if msg.type == "set_tempo":
             bpm = 60000000 / msg.tempo
             if current_time not in bpm_dict:
                 bpm_dict[current_time] = bpm
-        current_time += msg.time
+    if mid.type == 2:
+        bpm_list.append([{"time": time, "bpm": bpm} for time, bpm in bpm_dict.items()])
 
 
-bpm_list = [{"time": time, "bpm": bpm} for time, bpm in bpm_dict.items()]
-print(bpm_list)
+if mid.type != 2:
+    bpm_list = [[{"time": time, "bpm": bpm} for time, bpm in bpm_dict.items()] for _ in mid.tracks]
 
 print("获取音符时间...")
 for i, track in enumerate(mid.tracks):
@@ -107,9 +118,9 @@ for i, track in enumerate(mid.tracks):
         current_time += msg.time
         match msg.type:
             case "note_on":
-                if msg.note not in active_notes:
+                if msg.note not in active_notes and msg.velocity > 0:
                     active_notes[msg.note] = {
-                        "startTime": get_time(current_time)*1000,
+                        "startTime": get_time(current_time, i)*1000,
                         "note": msg.note
                     }
             case "note_off":
@@ -117,11 +128,11 @@ for i, track in enumerate(mid.tracks):
                     note = active_notes.pop(msg.note)
                     notes.append({
                         "startTime": note["startTime"],
-                        "endTime": get_time(current_time)*1000,
+                        "endTime": get_time(current_time, i)*1000,
                         "note": note["note"]
                     })
-        current_time_s = get_time(current_time)
-    max_length = max(current_time_s*1000+1000, max_length)
+        current_time_s = get_time(current_time, i)
+        max_length = max(current_time_s*1000+1000, max_length)
 print(f"谱面时长：{max_length}")
 
 notes.sort(key=lambda x: x["startTime"])
@@ -138,7 +149,28 @@ for i, note in enumerate(notes):
         "floorPosition": 2.2 * (note["startTime"]+one_note_use_time*j) / 1000
     } for j in range(math.ceil(use_time/one_note_use_time))])
 print("\n正在写入文件...")
-music = AudioSegment.silent(duration=max_length)
-music.export(os.path.join(sys.argv[2], "music.wav"), format="wav")
-with open(os.path.join(sys.argv[2], "chart.json"), "w") as f:
-    json.dump(chart, f)
+
+rand = random.randint(100000000, 999999999)
+music = AudioSegment.silent(duration=int(max_length))
+music.export(os.path.join(sys.argv[2], f"music{rand}.wav"), format="wav")
+img = Image.new("RGBA", (1920, 1080), (0, 0, 0, 255))
+font = ImageFont.truetype("./font.ttf", size=56)
+draw = ImageDraw.Draw(img)
+draw.text((960, 540), "这是一个使用Python生成的谱面", (255, 255, 255), font=font, align="center")
+img.save(os.path.join(sys.argv[2], f"bg{rand}.png"))
+with zipfile.ZipFile(os.path.join(sys.argv[2], "chart.zip"), "w", zipfile.ZIP_DEFLATED) as f:
+    f.writestr("0.json", json.dumps(chart).replace('"formatVersion": 3', '"formatVersion":3'))
+    f.write(os.path.join(sys.argv[2], f"bg{rand}.png"), "0.png")
+    f.write(os.path.join(sys.argv[2], f"music{rand}.wav"), "0.wav")
+    f.writestr("info.txt", """#
+Name: UK
+Path: 0
+Song: 0.wav
+Picture: 0.png
+Chart: 0.json
+Level: UK  Lv.10
+Composer: UK
+Charter: Python 3.12
+""")
+os.remove(os.path.join(sys.argv[2], f"bg{rand}.png"))
+os.remove(os.path.join(sys.argv[2], f"music{rand}.wav"))
